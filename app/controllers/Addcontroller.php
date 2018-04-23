f1e069e95ee3aea89ab79053273c8f98c53642c8
cayanat <cayanat.p@gmail.com>
2018-04-24 02:33:54 +0900

    66

--- app/controllers/Addcontroller.php
+++ app/controllers/Addcontroller.php
@@ -10,11 +10,30 @@ class AddController extends ControllerBase{
     $firstname = trim($this->request->getPost('eventdetail')); // รับค่าจาก form
     $photo = trim($this->request->getPost('photo')); // รับค่าจาก form
 
+    $photoUpdate='';
+	   if($this->request->hasFiles() == true){
+		   	$allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+			$uploads = $this->request->getUploadedFiles();
+		
+				$isUploaded = false;			
+				foreach($uploads as $upload){
+					if(in_array($upload->gettype(), $allowed)){					
+					 $photoName=md5(uniqid(rand(), true)).strtolower($upload->getname());
+					 $path = '../public/img/'.$photoName;
+					 ($upload->moveTo($path)) ? $isUploaded = true : $isUploaded = false;
+					}
+				}
+							 
+				if($isUploaded)  $photoUpdate=$photoName ;
+		}else
+				 die('You must choose at least one file to send. Please try again.');
+ 	
+
     $member=new Event();
     $member->eventname=$email;
     $member->eventdate=$pass;
     $member->eventdetail=$firstname;
-    $member->eventpicture=$photo;
+    $member->eventpicture=$photoUpdate;
     $member->save();
 
     $this->response->redirect('profile');
--- app/controllers/EditController.php
+++ app/controllers/EditController.php
@@ -15,6 +15,24 @@ class EditController extends ControllerBase{
           $pass = trim($this->request->getPost('eventdate')); // รับค่าจาก form
           $firstname = trim($this->request->getPost('eventdetail')); // รับค่าจาก form
           $photo = trim($this->request->getPost('photo')); // รับค่าจาก form
+          $photoUpdate='';
+          if($this->request->hasFiles() == true){
+              $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+            $uploads = $this->request->getUploadedFiles();
+          
+              $isUploaded = false;			
+              foreach($uploads as $upload){
+                if(in_array($upload->gettype(), $allowed)){					
+                $photoName=md5(uniqid(rand(), true)).strtolower($upload->getname());
+                $path = '../public/img/'.$photoName;
+                ($upload->moveTo($path)) ? $isUploaded = true : $isUploaded = false;
+                }
+              }
+                    
+              if($isUploaded)  $photoUpdate=$photoName ;
+          }else
+              die('You must choose at least one file to send. Please try again.');
+ 	
  
           
           $no = $_SESSION["edit"] ;
@@ -22,7 +40,7 @@ class EditController extends ControllerBase{
           $member->eventname=$email;
           $member->eventdate=$pass;
           $member->eventdetail=$firstname;
-          $member->eventpicture=$photo;
+          $member->eventpicture=$photoUpdate;
           $member->save();
       
           $this->response->redirect('profile');
