812a48d4c4063290a4232fee5e56861e1bbf9cf4
cayanat <cayanat.p@gmail.com>
2018-04-23 19:47:05 +0900

    add
    Signed-off-by: cayanat <cayanat.p@gmail.com>

---
+++ app/controllers/Addcontroller.php
@@ -0,0 +1,26 @@
+<?php
+use Phalcon\Mvc\View;
+class AddController extends ControllerBase{
+ 
+ public function indexAction(){
+  if($this->request->isPost()){
+    $email = trim($this->request->getPost('eventname')); // รับค่าจาก form
+    $pass = trim($this->request->getPost('eventdate')); // รับค่าจาก form
+    $firstname = trim($this->request->getPost('eventdetail')); // รับค่าจาก form
+    $photo = trim($this->request->getPost('photo')); // รับค่าจาก form
+
+    $member=new Event();
+    $member->eventname=$email;
+    $member->eventdate=$pass;
+    $member->eventdetail=$firstname;
+    $member->eventpicture=$photo;
+    $member->save();
+
+    $this->response->redirect('profile');
+
+    
+    }
+  
+}
+
+}
---
+++ app/controllers/EditController.php
@@ -0,0 +1,46 @@
+<?php
+use Phalcon\Mvc\View;
+class EditController extends ControllerBase{
+ 
+ 
+  public function setAction($num){
+    session_start();
+        $_SESSION["edit"] = $num;
+        if($this->request->isPost()){
+          $email = trim($this->request->getPost('eventname')); // รับค่าจาก form
+          $pass = trim($this->request->getPost('eventdate')); // รับค่าจาก form
+          $firstname = trim($this->request->getPost('eventdetail')); // รับค่าจาก form
+          $photo = trim($this->request->getPost('photo')); // รับค่าจาก form
+ 
+          
+          $no = $_SESSION["edit"] ;
+          $member = Event::findFirst("id = '$no'");
+          $member->eventname=$email;
+          $member->eventdate=$pass;
+          $member->eventdetail=$firstname;
+          $member->eventpicture=$photo;
+          $member->save();
+      
+          $this->response->redirect('profile');
+                
+          }
+  }
+  public function delAction($num){
+    session_start();
+        
+            
+          $email = trim($this->request->getPost('eventname')); // รับค่าจาก form
+          $pass = trim($this->request->getPost('eventdate')); // รับค่าจาก form
+          $firstname = trim($this->request->getPost('eventdetail')); // รับค่าจาก form
+          $photo = trim($this->request->getPost('photo')); // รับค่าจาก form
+
+          
+          $member = Event::findFirst("id = '$num'");
+          $member->delete();
+      
+          $this->response->redirect('profile');
+      
+          
+  }
+
+}
